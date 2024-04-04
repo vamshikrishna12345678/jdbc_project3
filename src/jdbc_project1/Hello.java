c9bbf6c4bd6ec74639062696d2ddc7ba34a26053
vamshikrishna12345678 <111067041+vamshikrishna12345678@users.noreply.github.com>
2024-04-04 14:42:29 +0530

    Update Hello.java

--- src/jdbc_project1/Hello.java
+++ src/jdbc_project1/Hello.java
@@ -3,7 +3,7 @@ package jdbc_project1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello welcome");
 		// TODO Auto-generated method stub
 
 	}
